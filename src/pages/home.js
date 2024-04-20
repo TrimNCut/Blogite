2952eb6f4afbb5b8849a66017b6b9d23648ef2cb
TrimNCut <daviestise@gmail.com>
2024-04-20 11:48:58 +0100

    Making Home Page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,17 @@
+import favicon from '../images/favicon.png';
+
 function Home(){
     return(
-        <h1>Home</h1>
+        <div className="home">
+            <div className="hero">
+                <div>
+                    <img src={favicon} alt="Logo"/>
+                    <div>
+                        <h1>Blogging,<br/>But With A Twist</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
